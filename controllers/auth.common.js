20c7b16dfe3cd1082c8fd1f895e4f8a9cec5e58d
sknah <sangkwon@79>
2017-10-04 17:58:58 +0900

    - modify

--- controllers/auth.common.js
+++ controllers/auth.common.js
@@ -17,15 +17,15 @@ const authentication = (req, res, next) => {
 	}
 	const authPromise = new Promise(
 		(resolve, reject) => {
-			// jwt.verify(token, secret, (err, deccodedToken) => {
-			// 	if(err) reject(err)
-			// 	resolve(deccodedToken)
-			// })
-			if(token.indexOf('omg')>=0) {
-				resolve({secret,'isomg' : 'omgggg'});
-			}else {
-				reject({token, error: 'fucked'})
-			}
+			jwt.verify(token, secret, (err, deccodedToken) => {
+				if(err) reject(err)
+				resolve(deccodedToken)
+			})
+			// if(token.indexOf('omg')>=0) {
+			// 	resolve({secret,'isomg' : 'omgggg'});
+			// }else {
+			// 	reject({token, error: 'fucked'})
+			// }
 		}
 	);
 	
