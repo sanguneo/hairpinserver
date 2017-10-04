92d7318274332e52cc2b00d1e71f0717083c6d43
sknah <sangkwon@79>
2017-10-04 17:27:46 +0900

    - modify

--- controllers/auth.common.js
+++ controllers/auth.common.js
@@ -36,7 +36,7 @@ const authentication = (req, res, next) => {
 		service: 'user',
 		function: 'basic',
 		message: 'Authorization failed.',
-		reqapp: req.app.secretnipriah,
+		reqapp: req.app,
 		...error,
 	}));
 }
