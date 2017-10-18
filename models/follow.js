7ce2e8cad30d1a93c25abeeae091575db4d7c59a
sanguneo <sknah0319@gmail.com>
2017-10-18 14:27:28 +0900

    - user

--- models/follow.js
+++ models/follow.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 
 const sFollow = mongoose.Schema({
-	fid			: {type: ObjectId, required: false, unique: true},
-	signhash	: {type: String, required: true, unique: true},
+	fid			: {type: ObjectId},
+	signhash	: {type: String},
 });
 
 module.exports = {sFollow,mFollow: mongoose.model('mFollow', sFollow)};
