e6b579004b715a00e05932f582cf8478bf5f3561
sanguneo <sknah0319@gmail.com>
2017-10-26 12:16:44 +0900

    - notice 추가

--- controllers/notice.js
+++ controllers/notice.js
@@ -45,7 +45,7 @@ module.exports = (express) => {
 	}).all((req, res) => res.jsonp({ code: 339, service: 'notice', function: 'listup', message: 'unauthorized_method' }));
 
 	router.route('/plain').get((req, res) => {
-		mNotice.find([{noticeType: 0}, {noticeType: undefined}],['regDate', 'noticeType', 'content'],{sort: {regDate: -1}},(error, notice) => {
+		mNotice.find({noticeType: { $exists: 1, $ne: null }},['regDate', 'noticeType', 'content'],{sort: {regDate: -1}},(error, notice) => {
 			if(error) {
 				return res.jsonp({ code: 338, service: 'notice', function: 'listup', message: 'error', error });
 			}
