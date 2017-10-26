25449c8be9a05a3e3c6ac9670b8936658a9b1869
sanguneo <sanguneo@users.noreply.github.com>
2017-10-26 19:15:22 +0900

    Update notice.js
    omg

--- controllers/notice.js
+++ controllers/notice.js
@@ -13,7 +13,7 @@ module.exports = (express) => {
 	    return res.jsonp({
 	        name: 'hair.pin notice service API',
 	        version: '1.0.0',
-			date: Date.now()
+		date: new Date(Date.now()).getHours()
 	    });
 	});
 
