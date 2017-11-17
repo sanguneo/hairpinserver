d7fe6b57e6b32039a56f8f31b661cb946f1818e0
sanguneo <sknah0319@gmail.com>
2017-11-17 14:46:44 +0900

    - notice get one

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-var http = require('http');
-var app = require('./app');
-var port = process.env.PORT || 4337;
-var host = process.env.HOST || '0.0.0.0';
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const app = require('./app');
+const port = process.env.PORT || 4337;
+const portssl = process.env.PORTSSL || 4443;
+const host = process.env.HOST || '0.0.0.0';
+
+const options = {
+	key: fs.readFileSync('./cert/privkey.pem'),
+	cert: fs.readFileSync('./cert/cert.pem')
+};
 
 http.createServer(app).listen(port, host, () => {
 	console.log('Server ready at http://' + host + ':' + port);
+});
+https.createServer(options, app).listen(portssl, host, () => {
+	console.log('Server ready at https://' + host + ':' + portssl);
 });
