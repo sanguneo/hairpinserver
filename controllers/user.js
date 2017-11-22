6575ab3b8137423c62aaca5617bb2816a50405e9
sanguneo <sknah0319@gmail.com>
2017-11-22 14:12:51 +0900

    - search like

--- controllers/user.js
+++ controllers/user.js
@@ -222,12 +222,12 @@ module.exports = (express, passport) => {
 	router.route(['/searchuser/:param', '/searchuser/']).get((req, res) => {
 		let {param} = req.params;
 		let _id = req.decoded._id;
-				const query = (!param || param === '') ? {} : {$or: [{nickname: {$regex: '.*' + param +'.*'}}, {email: {$regex: '.*' + param +'.*'}}]};
+		const query = (!param || param === '') ? {} : {$or: [{nickname: {$regex: '.*' + param +'.*'}}, {email: {$regex: '.*' + param +'.*'}}]};
 		mUser.find(query,['_id', 'signhash', 'nickname', 'email'],(error, user) => {
 			if(error) {
 				return res.jsonp({ code: 278, service: 'user', function: 'userstat', message: 'error', error });
 			}
-			return res.jsonp({ code: 270, service: 'user', function: 'userstat', message: 'success', id: _id,user: user.filter((e) => e._id !== _id)});
+			return res.jsonp({ code: 270, service: 'user', function: 'userstat', message: 'success', id: _id,user: user.filter((e) => e._id != _id)});
 		})
 	}).all((req, res) => res.jsonp({ code: 279, service: 'user', function: 'userstat', message: 'unauthorized_method' }));
 
