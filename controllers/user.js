722f8aa1bd2c965adba24c927dd70604426088d5
sanguneo <sknah0319@gmail.com>
2017-11-23 18:05:41 +0900

    카운트 서치

--- controllers/user.js
+++ controllers/user.js
@@ -213,7 +213,7 @@ module.exports = (express, passport) => {
 		mUser.aggregate(query).exec((error, tagswrap) => {
 			let tags = tagswrap.filter((e) => e._id === param);
 			if(error) return res.jsonp({ code: 278, service: 'searchtag', function: 'userstat', message: 'error', error });
-			if (tags.length) res.jsonp({ code: 271, service: 'searchtag', function: 'userstat', message: 'notags', tags});
+			if (tags.length <= 0) res.jsonp({ code: 271, service: 'searchtag', function: 'userstat', message: 'notags', tags: []});
 			return res.jsonp({ code: 270, service: 'searchtag', function: 'userstat', message: 'success', tags: tags.map((tag) => {return {_id: tag._id, signhash: tag.signhash[0], count: tag.count}})});
 		})
 
