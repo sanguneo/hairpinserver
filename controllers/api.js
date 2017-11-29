bc6101d9ba69188e989d677e897f6251c526d32d
sanguneo <sknah0319@gmail.com>
2017-11-30 08:40:54 +0900

    - cors 임시 제거

--- controllers/api.js
+++ controllers/api.js
@@ -7,7 +7,7 @@ module.exports = express => {
 		// res.header('Access-Control-Allow-Origin', '*');
 		res.header(
 			'Access-Control-Allow-Headers',
-			'Origin, X-Requested-With, Content-Type, Accept'
+			'X-Requested-With, Content-Type, Accept'
 		);
 		next();
 	});
--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,7 @@ module.exports = (express, passport) => {
 	
 	router.use((req, res, next) => {
 	    // res.header("Access-Control-Allow-Origin", "*");
-	    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, nekotnipriah");
+	    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, nekotnipriah");
 		res.header("charset=utf-8");
 	    next();
 	});
