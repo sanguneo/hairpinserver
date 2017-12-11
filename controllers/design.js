449bfc437a48a3d9e153c24a29378fd2c2b04b03
sanguneo <sknah0319@gmail.com>
2017-12-11 18:02:42 +0900

    - design uploaded

--- controllers/design.js
+++ controllers/design.js
@@ -35,9 +35,10 @@ module.exports = (express) => {
 		if (!designhash) {
 			return res.jsonp({ code: 406, service: 'design', function: 'upload', message: 'unsatisfied_param'});
 		}
+		console.log(123123, req.files,req.body.files);
 		['SRC_LEFT', 'SRC_RIGHT','ORG','THUMB'].forEach((item) => {
 			// fsSettings(req[item], signhash + '_' + designhash + '_' + item);
-			console.log(req.files);
+
 		});
 		return res.jsonp({ code: 400, service: 'design', function: 'upload', message: 'success', signhash});
 	}).all((req, res) => res.jsonp({code: 409, service: 'design', function: 'upload', message: 'unauthorized_method'}));
