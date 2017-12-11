e44486faad92f53ebeda650f07350da54c8c3855
sanguneo <sknah0319@gmail.com>
2017-12-12 08:46:50 +0900

    - design uploaded

--- controllers/design.js
+++ controllers/design.js
@@ -28,17 +28,24 @@ module.exports = (express) => {
 	const fsSettings = (file, filename)=> {
 		(file && file.path) && fs.copyFile(file.path, uploadPath + '/' + filename, ()=> fs.unlinkSync(file.path));
 	}
-
+/* { fieldname: 'designimage',
+    originalname: 'f8a3aff8-8b2e-5a07-8717-327fa422dbf6_11511121918111514111410171_THUMB.scalb',
+    encoding: '7bit',
+    mimetype: 'image/jpeg',
+    destination: 'upload/designs',
+    filename: 'e5b5a3ba61b052508d5e0e3cdd90966c',
+    path: 'upload/designs/e5b5a3ba61b052508d5e0e3cdd90966c',
+    size: 8507 }
+* */
 	router.route('/upload').post(profileUpload.array('designimage', 4), (req, res) => {
-		console.log(req.files, req.file, 123123123123);
 		const {signhash} = req.decoded;
 		const {designhash} = req.body;
 		if (!designhash) {
 			return res.jsonp({ code: 406, service: 'design', function: 'upload', message: 'unsatisfied_param'});
 		}
-
-		['SRC_LEFT', 'SRC_RIGHT','ORG','THUMB'].forEach((item) => {
-		});
+		req.files.forEach((file) => {
+			fs.renameSync(file.destination + '/' + filename, file.destination + '/' + file.originalname)
+		})
 		return res.jsonp({ code: 400, service: 'design', function: 'upload', message: 'success', signhash});
 	}).all((req, res) => res.jsonp({code: 409, service: 'design', function: 'upload', message: 'unauthorized_method'}));
 
