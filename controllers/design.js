e1108e1684394cf5f57513c8bd80fa545d6b252d
sanguneo <sknah0319@gmail.com>
2017-12-13 16:02:25 +0900

    - design get tag

--- controllers/design.js
+++ controllers/design.js
@@ -82,7 +82,7 @@ module.exports = (express) => {
 			if(err) res.jsonp({ code: 408, service: 'design', function: 'tags', message: 'error', error: err});
 			const tagList = {};
 			designs.forEach(({signhash, tags}) => {
-				mUser.findOne({signhash}, ['following'] ,(error, followings) => {
+				mUser.findOne({signhash},(error, followings) => {
 					console.log(followings);
 				});
 				tags.forEach((tag) => tagList[tag] = (tagList[tag] ? tagList[tag] + 1 : 1))
