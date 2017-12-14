1cb0c6d7fd7e94b9c76daf1dffa4ac6bad8b40a6
sanguneo <sknah0319@gmail.com>
2017-12-14 18:19:51 +0900

    - design get design

--- controllers/design.js
+++ controllers/design.js
@@ -111,7 +111,9 @@ module.exports = (express) => {
 
 	router.route(['/getdesign']).post((req, res) => {
 		const {designHash, signhash} = req.body;
+
 		if (req.decoded.signhash || !designHash) res.jsonp({code: 426, service: 'user', function: 'signup', message: 'unsatisfied_param'});
+
 		mDesign.findOne({signhash, designHash}, function(err, design) {
 			if (err) res.jsonp({code: 428, service: 'design', function: 'getdesign', message: 'error', error: err});
 			if (design) res.jsonp({ code: 420, service: 'design', function: 'getdesign', message: 'success', design});
