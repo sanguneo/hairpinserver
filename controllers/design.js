b6863df9c55f2afb40f195da38721926e4f81ab0
sanguneo <sknah0319@gmail.com>
2017-12-14 13:57:16 +0900

    - design get design

--- controllers/design.js
+++ controllers/design.js
@@ -77,16 +77,37 @@ module.exports = (express) => {
 		const myhash = req.decoded.signhash;
 		query.$or.push({signhash: myhash});
 		mDesign.find(query,['signhash', 'tags', 'publish'], function(err, designs) {
-			if(err) res.jsonp({ code: 418, service: 'design', function: 'tags', message: 'error', error: err});
+			if(err) res.jsonp({ code: 408, service: 'design', function: 'tags', message: 'error', error: err});
 			const tagList = {};
 			designs.forEach(({signhash, tags, publish}) => {
 				if(signhash === myhash || publish === 7) tags.forEach((tag) => tagList[tag] = (tagList[tag] ? tagList[tag] + 1 : 1));
 				else if (publish === 3) mUser.findOne({signhash},['following'],(error, {following}) => {
 					following.includes(myhash) && tags.forEach((tag) => tagList[tag] = (tagList[tag] ? tagList[tag] + 1 : 1))});
 			});
-			setTimeout(()=> res.jsonp({ code: 410, service: 'design', function: 'tags', message: 'success', tags: tagList, signhash: myhash}),500);
+			setTimeout(()=> res.jsonp({ code: 400, service: 'design', function: 'tags', message: 'success', tags: tagList, signhash: myhash}),500);
 		});
-	}).all((req, res) => res.jsonp({code: 419, service: 'design', function: 'tags', message: 'unauthorized_method'}));
+	}).all((req, res) => res.jsonp({code: 409, service: 'design', function: 'tags', message: 'unauthorized_method'}));
+
+	router.route(['/designs', '/designs/:permission']).get((req, res) => {
+		const {permission} = req.params;
+		let query = !permission ? {$or: [{publish: 7}]} : {$or: [{publish: permission}, {publish: 7}]};
+		var myhash;
+		if (req.decoded) {
+			myhash = req.decoded.signhash;
+			query.$or.push({signhash: myhash});
+		}
+		mDesign.find(query,['signhash', 'designHash', 'title', 'regDate', 'publish'], function(err, designs) {
+			if(err) res.jsonp({ code: 418, service: 'design', function: 'designs', message: 'error', error: err});
+			const designList = [];
+			designs.forEach(({signhash, designHash, title, regDate, publish}) => {
+				mUser.findOne({signhash},['nickname', 'following'],(error, {nickname, following}) => {
+					if(signhash === myhash || publish === 7) designList.push({nickname, signhash, designHash, title, regDate, publish})
+					else if (publish === 3) following.includes(myhash) && designList.push({nickname, signhash, designHash, title, regDate, publish});
+				});
+			});
+			setTimeout(()=> res.jsonp({ code: 410, service: 'design', function: 'designs', message: 'success', designs: designList, signhash: myhash}),500);
+		});
+	}).all((req, res) => res.jsonp({code: 419, service: 'design', function: 'designs', message: 'unauthorized_method'}));
 
 	router.route(['/getdesign']).get((req, res) => {
 		const {designHash, signhash} = req.body;
@@ -95,10 +116,8 @@ module.exports = (express) => {
 			if (err) res.jsonp({code: 428, service: 'design', function: 'getdesign', message: 'error', error: err});
 			if (design) res.jsonp({ code: 420, service: 'design', function: 'getdesign', message: 'success', design});
 			else res.jsonp({ code: 427, service: 'design', function: 'getdesign', message: 'design_notexist'});
-
 		});
 	}).all((req, res) => res.jsonp({code: 429, service: 'design', function: 'getdesign', message: 'unauthorized_method'}));
-
 
 	return router;
 };
